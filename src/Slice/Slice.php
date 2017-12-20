9af100a725e74da2b032e860b6520538fd27065b
Babichev Maxim <maksim.babichev95@gmail.com>
2017-12-20 16:44:15 +0300

    Update Slice.php

--- src/Slice/Slice.php
+++ src/Slice/Slice.php
@@ -79,7 +79,8 @@ class Slice extends Iterator
 
         Arr::walkRecursive($this->data, function (&$value) use ($slice)
         {
-            if (\is_string($value) && $value{0} === '%' &&
+            if (!empty($value) && 
+                \is_string($value) && $value{0} === '%' &&
                 $value{\strlen($value) - 1} === '%' &&
                 \substr_count($value, '%') === 2)
             {
